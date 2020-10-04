d80504852a88e60620f088120deaabfec85ab3b3
David Ortner <david@ortner.se>
2020-10-04 22:38:25 +0200

    #60@minor: Adds integration test for Vue.

---
+++ packages/jest-environment/test/vue/Vue.test.ts
@@ -0,0 +1,26 @@
+import Vue from 'vue/dist/vue.common.prod.js';
+
+describe('Vue', () => {
+	let appElement: Element;
+
+	beforeEach(() => {
+		appElement = document.createElement('div');
+		appElement.id = 'app';
+		appElement.innerHTML = '{{ message }}';
+		document.body.appendChild(appElement);
+	});
+
+	afterEach(() => {
+		document.body.innerHTML = '';
+	});
+
+	test('Tests integration with Vue.', () => {
+		new Vue({
+			el: '#app',
+			data: {
+				message: 'Test'
+			}
+		});
+		expect(document.body.innerHTML).toBe('<div id="app">Test</div>');
+	});
+});
