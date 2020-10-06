961141d99820d050d36fc141dc540ee62d3e4866
David Ortner <david@ortner.se>
2020-10-07 01:53:03 +0200

    #97@patch: Nodes that are outside of the `<html>` tag when parsing a page should be added to the `<body>` element. This patch will fix this issue.

--- packages/happy-dom/test/dom-parser/DOMParser.test.ts
+++ packages/happy-dom/test/dom-parser/DOMParser.test.ts
@@ -14,13 +14,41 @@ describe('DOMParser', () => {
 			const newDocument = domParser.parseFromString(DOMParserHTML, 'text/html');
 			expect(newDocument.documentElement.outerHTML).toBe(DOMParserHTML);
 		});
-	});
 
-	describe('parseFromString()', () => {
 		test('Parses HTML and returns a new document.', () => {
 			const newDocument = domParser.parseFromString('Test', 'text/html');
 			expect(newDocument.documentElement.outerHTML).toBe(
 				'<html><head></head><body>Test</body></html>'
+			);
+		});
+
+		test('Adds elements outside of the <html> tag to the <body> tag.', () => {
+			const newDocument = domParser.parseFromString(
+				`
+				<html>
+					<head>
+						<title>Title</title>
+					</head>
+					<body>
+						<span>Body</span>
+					</body>
+				</html>
+				<div>Should be added to body</div>
+			`,
+				'text/html'
+			);
+			expect(newDocument.documentElement.outerHTML.replace(/[\s]/gm, '')).toBe(
+				`
+				<html>
+					<head>
+						<title>Title</title>
+					</head>
+					<body>
+						<span>Body</span>
+						<div>Should be added to body</div>
+					</body>
+				</html>
+				`.replace(/[\s]/gm, '')
 			);
 		});
 	});
