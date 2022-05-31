d40983d02634b1d31885bd4357ff5d14a4f465a0
rudywaltz <david.barkoczi@gmail.com>
2022-05-31 13:24:15 +0200

    #475@minor: Adds support for HTMLMediaElement.

---
+++ packages/happy-dom/src/nodes/html-audio-element/HTMLAudioElement.ts
@@ -0,0 +1,11 @@
+import HTMLMediaElement from '../html-media-element/HTMLMediaElement';
+import IHTMLAudioElement from './IHTMLAudioElement';
+
+/**
+ * HTML Audio Element.
+ *
+ * Reference:
+ * https://developer.mozilla.org/en-US/docs/Web/API/HTMLAudioElement.
+ *
+ */
+export default class HTMLAudioElement extends HTMLMediaElement implements IHTMLAudioElement {}
