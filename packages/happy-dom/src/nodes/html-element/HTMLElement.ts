0365a44a71f3db9d7ccb4b3cb75b35200ede1968
takaya1992 <tky.c10ver@gmail.com>
2023-01-06 10:56:16 +0900

    #652@patch: Fixed deleting not exist dataset properties.

--- packages/happy-dom/src/nodes/html-element/HTMLElement.ts
+++ packages/happy-dom/src/nodes/html-element/HTMLElement.ts
@@ -251,10 +251,9 @@ export default class HTMLElement extends Element implements IHTMLElement {
 			},
 			deleteProperty: (dataset: { [key: string]: string }, key: string) => {
 				const name = 'data-' + DatasetUtility.camelCaseToKebab(key);
-				const exists = !!attributes[name];
-				delete attributes[name];
-				delete dataset[key];
-				return exists;
+				const result1 = delete attributes[name];
+				const result2 = delete dataset[key];
+				return result1 && result2;
 			},
 			ownKeys: (dataset: { [key: string]: string }) => {
 				// According to Mozilla we have to update the dataset object (target) to contain the same keys as what we return:
