e3b7e5b124ca610c3c94e018f7f4d2d4ef66bb59
yangxingzhou <yangxingzhou@shein.com>
2023-01-19 15:25:29 +0800

    #693@patch: Fixed @supports not pasre to CSSSupportsRule.

---
+++ packages/happy-dom/src/css/rules/CSSSupportsRule.ts
@@ -0,0 +1,23 @@
+import CSSRule from '../CSSRule';
+
+/**
+ * CSSRule interface.
+ */
+export default class CSSSupportsRule extends CSSRule {
+	public readonly type = CSSRule.SUPPORTS_RULE;
+	public readonly cssRules: CSSRule[] = [];
+	public readonly conditionText = '';
+
+	/**
+	 * Returns css text.
+	 *
+	 * @returns CSS text.
+	 */
+	public get cssText(): string {
+		let cssText = '';
+		for (const cssRule of this.cssRules) {
+			cssText += cssRule.cssText;
+		}
+		return `@supports ${this.conditionText} { ${cssText} }`;
+	}
+}
