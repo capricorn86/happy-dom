e704aca798640671ca01791282bff35f5eb2f1a7
Tanguy Krotoff <tkrotoff@gmail.com>
2023-09-24 16:50:56 +0200

    #1092@patch: Simplifies InputEvent.
    https://w3c.github.io/uievents/#interface-inputevent.

--- packages/happy-dom/src/event/events/InputEvent.ts
+++ packages/happy-dom/src/event/events/InputEvent.ts
@@ -6,10 +6,10 @@ import IInputEventInit from './IInputEventInit.js';
  *
  */
 export default class InputEvent extends UIEvent {
-	public readonly data: string = '';
-	public readonly dataTransfer: DataTransfer = null;
-	public readonly inputType: string = '';
-	public readonly isComposing: boolean = false;
+	public readonly data: string;
+	public readonly dataTransfer: DataTransfer;
+	public readonly inputType: string;
+	public readonly isComposing: boolean;
 
 	/**
 	 * Constructor.
@@ -17,14 +17,12 @@ export default class InputEvent extends UIEvent {
 	 * @param type Event type.
 	 * @param [eventInit] Event init.
 	 */
-	constructor(type: string, eventInit: IInputEventInit = null) {
+	constructor(type: string, eventInit: IInputEventInit = {}) {
 		super(type, eventInit);
 
-		if (eventInit) {
-			this.data = eventInit.data || '';
-			this.dataTransfer = eventInit.dataTransfer || null;
-			this.inputType = eventInit.inputType || '';
-			this.isComposing = eventInit.isComposing || false;
-		}
+		this.data = eventInit.data ?? '';
+		this.dataTransfer = eventInit.dataTransfer ?? null;
+		this.inputType = eventInit.inputType ?? '';
+		this.isComposing = eventInit.isComposing ?? false;
 	}
 }
