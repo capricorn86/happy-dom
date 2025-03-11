7e3845e7f0b24058d2f8f9780d84d720766f850b
Thomas Portelange <thomas@lekoala.be>
2025-03-11 15:27:43 +0100

    set timeout to 0
    Without this, I get a TimeoutNaNWarning: NaN is not a number. (with bun)

--- packages/happy-dom/src/window/BrowserWindow.ts
+++ packages/happy-dom/src/window/BrowserWindow.ts
@@ -1335,7 +1335,7 @@ export default class BrowserWindow extends EventTarget implements INodeJSGlobal 
 							timeout.callback();
 						}
 					}
-				});
+				}, 0);
 
 				zeroDelayTimeout.timeouts = [];
 				this.#browserFrame[PropertySymbol.asyncTaskManager].startTimer(id);
