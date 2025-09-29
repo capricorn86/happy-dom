503b7762fe00d49acaa1f9797ed3d8d30fa7d7cd
David Ortner <david@ortner.se>
2025-09-30 00:21:17 +0200

    fix: [#1620] Fixes issue related to CSS pseudo selector :scope

--- packages/happy-dom/src/query-selector/QuerySelector.ts
+++ packages/happy-dom/src/query-selector/QuerySelector.ts
@@ -74,7 +74,7 @@ export default class QuerySelector {
 	 * @returns HTML elements.
 	 */
 	public static querySelectorAll(
-		node: Element | Document | DocumentFragment,
+		node: Element | DocumentFragment,
 		selector: string
 	): NodeList<Element> {
 		const window = node[PropertySymbol.window];
@@ -115,7 +115,11 @@ export default class QuerySelector {
 			}
 		}
 
-		const groups = SelectorParser.getSelectorGroups(selector, { scope: node });
+		const scope =
+			node[PropertySymbol.nodeType] === NodeTypeEnum.documentNode
+				? (<Document>node).documentElement
+				: node;
+		const groups = SelectorParser.getSelectorGroups(selector, { scope });
 		const items: Element[] = [];
 		const nodeList = new NodeList<Element>(PropertySymbol.illegalConstructor, items);
 		const matchesMap: Map<string, Element> = new Map();
@@ -251,7 +255,11 @@ export default class QuerySelector {
 
 		const matchesMap: Map<string, Element> = new Map();
 		const matchedPositions: string[] = [];
-		for (const items of SelectorParser.getSelectorGroups(selector, { scope: node })) {
+		const scope =
+			node[PropertySymbol.nodeType] === NodeTypeEnum.documentNode
+				? (<Document>node).documentElement
+				: node;
+		for (const items of SelectorParser.getSelectorGroups(selector, { scope })) {
 			const match =
 				node[PropertySymbol.nodeType] === NodeTypeEnum.elementNode
 					? this.findFirst(<Element>node, [<Element>node], items, cachedItem)
@@ -284,7 +292,7 @@ export default class QuerySelector {
 	public static matches(
 		element: Element,
 		selector: string,
-		options?: { scope?: Element | Document | DocumentFragment | null; ignoreErrors?: boolean }
+		options?: { scope?: Element | DocumentFragment | null; ignoreErrors?: boolean }
 	): ISelectorMatch | null {
 		const ignoreErrors = options?.ignoreErrors;
 		const window = element[PropertySymbol.window];
@@ -350,9 +358,14 @@ export default class QuerySelector {
 			);
 		}
 
+		const scopeOrElement = options?.scope || element;
+		const scope =
+			scopeOrElement[PropertySymbol.nodeType] === NodeTypeEnum.documentNode
+				? (<Document>scopeOrElement).documentElement
+				: scopeOrElement;
 		for (const items of SelectorParser.getSelectorGroups(selector, {
 			...options,
-			scope: options?.scope || element
+			scope
 		})) {
 			const result = this.matchSelector(element, items.reverse(), cachedItem);
 
--- packages/happy-dom/src/query-selector/SelectorParser.ts
+++ packages/happy-dom/src/query-selector/SelectorParser.ts
@@ -72,13 +72,16 @@ export default class SelectorParser {
 	 *
 	 * @param selector Selector.
 	 * @param options Options.
-	 * @param options.scope Scope.
+	 * @param [options.scope] Scope.
 	 * @param [options.ignoreErrors] Ignores errors.
 	 * @returns Selector item.
 	 */
 	public static getSelectorItem(
 		selector: string,
-		options?: { scope?: Element | Document | DocumentFragment; ignoreErrors?: boolean }
+		options?: {
+			scope?: Element | DocumentFragment;
+			ignoreErrors?: boolean;
+		}
 	): SelectorItem {
 		return this.getSelectorGroups(selector, options)[0][0];
 	}
@@ -88,13 +91,16 @@ export default class SelectorParser {
 	 *
 	 * @param selector Selector.
 	 * @param options Options.
-	 * @param options.scope Scope.
+	 * @param [options.scope] Scope.
 	 * @param [options.ignoreErrors] Ignores errors.
 	 * @returns Selector groups.
 	 */
 	public static getSelectorGroups(
 		selector: string,
-		options?: { scope?: Element | Document | DocumentFragment; ignoreErrors?: boolean }
+		options?: {
+			scope?: Element | DocumentFragment;
+			ignoreErrors?: boolean;
+		}
 	): Array<Array<SelectorItem>> {
 		selector = selector.trim();
 		const ignoreErrors = options?.ignoreErrors;
@@ -296,15 +302,18 @@ export default class SelectorParser {
 	 *
 	 * @param name Pseudo name.
 	 * @param args Pseudo arguments.
-	 * @param options Options.
-	 * @param options.scope Scope.
+	 * @param [options] Options.
+	 * @param [options.scope] Scope.
 	 * @param [options.ignoreErrors] Ignores errors.
 	 * @returns Pseudo.
 	 */
 	private static getPseudo(
 		name: string,
 		args: string | null | undefined,
-		options?: { scope?: Element | Document | DocumentFragment; ignoreErrors?: boolean }
+		options?: {
+			scope?: Element | DocumentFragment;
+			ignoreErrors?: boolean;
+		}
 	): ISelectorPseudo {
 		const lowerName = name.toLowerCase();
 
