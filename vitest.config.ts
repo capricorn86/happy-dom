81a877d33520691d151d22f542d6ea1c5a41d578
David Ortner <david@ortner.se>
2026-01-22 23:53:12 +0100

    chore: [#2032] Fixes vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	test: {
-		projects: ['./packages/happy-dom/vitest.config.ts']
+		projects: ['./packages/happy-dom/', './packages/@happy-dom/server-renderer/']
 	}
 });
